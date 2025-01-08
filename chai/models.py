1d479a4a0c805f6cbcaa32699277901a8f71844a
Mrudul Chaudhari <mrudulchaudhari0608@gmail.com>
2025-01-09 00:53:11 +0530

    added own db

--- chai/models.py
+++ chai/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class ChaiVariety(models.Model):  # Capital C and V
+    CHAI_TYPE_CHOICE = [
+        ('ML', 'MASALA'),
+        ('GR', 'GINGER'),
+        ('PL', 'PLAIN'),
+        ('KL', 'KIWI'),
+        ('EL', 'ELAICHI'),
+    ]
+    
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to="chais/")
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICE, default='PL')
+
+    def __str__(self):
+        return self.name
