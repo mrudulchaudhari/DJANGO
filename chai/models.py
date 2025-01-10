864315ca87cb19360e8dd7e3036ba364c32f53db
Mrudul Chaudhari <mrudulchaudhari0608@gmail.com>
2025-01-10 16:32:46 +0530

    added price feature

--- chai/models.py
+++ chai/models.py
@@ -15,6 +15,7 @@ class ChaiVariety(models.Model):  # Capital C and V
     date_added = models.DateTimeField(default=timezone.now)
     type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICE, default='PL')
     description = models.TextField(default='')
+    price = models.TextField(default = '')
 
     def __str__(self):
         return self.name
