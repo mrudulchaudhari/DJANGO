e4e6d1a9fbdb25071a4eb9f830e0d39484562c5c
Mrudul Chaudhari <mrudulchaudhari0608@gmail.com>
2025-01-10 16:21:52 +0530

    added description page

--- chai/models.py
+++ chai/models.py
@@ -14,6 +14,7 @@ class ChaiVariety(models.Model):  # Capital C and V
     image = models.ImageField(upload_to="chais/")
     date_added = models.DateTimeField(default=timezone.now)
     type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICE, default='PL')
+    description = models.TextField(default='')
 
     def __str__(self):
         return self.name
--- chai/urls.py
+++ chai/urls.py
@@ -19,5 +19,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_chai, name='all_home'),
-
+    path('<int:chai_id>/', views.chai_details, name='chai_detail')
 ]
