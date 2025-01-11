60b35252cce77270ce65674236f59bf049bb61b7
Mrudul Chaudhari <mrudulchaudhari0608@gmail.com>
2025-01-11 13:37:26 +0530

    added new classes in models.py

--- chai/admin.py
+++ chai/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import ChaiVariety
+from .models import ChaiVariety, ChaiReview, Store, ChaiCertificate
 
 admin.site.register(ChaiVariety)
--- chai/models.py
+++ chai/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
-class ChaiVariety(models.Model):  # Capital C and V
+class ChaiVariety(models.Model):
     CHAI_TYPE_CHOICE = [
         ('ML', 'MASALA'),
         ('GR', 'GINGER'),
@@ -15,7 +16,37 @@ class ChaiVariety(models.Model):  # Capital C and V
     date_added = models.DateTimeField(default=timezone.now)
     type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICE, default='PL')
     description = models.TextField(default='')
-    price = models.TextField(default = '')
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # Changed from TextField to DecimalField
 
     def __str__(self):
         return self.name
+    
+#One to Many
+class ChaiReview(models.Model):
+    chai = models.ForeignKey(ChaiVariety, on_delete=models.CASCADE, related_name="reviews")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Fixed this line
+    rating = models.IntegerField()
+    comment = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.user.username} review for {self.chai.name}'
+
+#Many To Many
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    chai_varities = models.ManyToManyField(ChaiVariety, related_name="stores")
+
+    def __str__(self):
+        return self.name
+    
+#One to One
+class ChaiCertificate(models.Model):
+    chai = models.OneToOneField(ChaiVariety, on_delete=models.CASCADE, related_name="Certificate")
+    certificate_number = models.CharField(max_length=100)
+    issued_date = models.DateTimeField(default=timezone.now)
+    valid_until = models.DateTimeField()
+
+    def __str__(self):
+        return f'Certificate for {self.chai.name}'
